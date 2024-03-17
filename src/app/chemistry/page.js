0992d351655161ce2b69675bfc7c564bbf71c7d0
Adarsh Vishwakarma <vish.adarsh01@gmail.com>
2024-03-17 23:13:58 +0530

    fixed the error the was causing deployment issue

--- src/app/chemistry/page.js
+++ src/app/chemistry/page.js
@@ -5,9 +5,9 @@ const Page = () => {
     <div className='containered h-[100%] h-screen'>
             <div className="h-[100vh] pt-20" >
               <div className='bg-sky-300 grid grid-cols-2 gap-4 h-[17rem] p-8 pb-8 rounded-3xl w-[40rem] ml-2 justify-center items-center ml-[30%] '>
-              <button className="button"><Link href='/buffer'>Buffer Solution</Link></button>
-              <button className="button"><Link href='/balancer'>Equation Balancing</Link></button>
-              <button className="button"><Link href='/equilibrium'>Equillibrium</Link></button>
+              <button className="button"><Link href='chemistry/buffer'>Buffer Solution</Link></button>
+              <button className="button"><Link href='chemistry/balancer'>Equation Balancing</Link></button>
+              <button className="button"><Link href='chemistry/equillibrium'>Equillibrium</Link></button>
               <button className="button"><Link href=''>Coming..</Link></button>
               </div>
             </div>
